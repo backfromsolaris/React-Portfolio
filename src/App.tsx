3ded45e040278b762e9eca56ab4f4023bb2e2b31
backfromsolaris <porraszach@gmail.com>
2021-08-22 10:30:55 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const myApp = () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigation">
+        <img src={logo} className="logo" alt="My Logo"></img>
+        <div className="navigation-sub">
+          <a href="" className="item">About</a>
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">Contact</a>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default myApp;
